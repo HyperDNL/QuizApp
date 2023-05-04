c997a630492d943587ccb91371e8c2e14e9aa616
Daniel López <hyperdaniel579exe@gmail.com>
2023-05-04 17:22:47 -0600

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Daniel López on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

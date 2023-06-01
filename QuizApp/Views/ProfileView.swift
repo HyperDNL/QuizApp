3c5c2b6639d5c4a0747b1a04fe68a7a9d70c1f92
Daniel López <hyperdaniel579exe@gmail.com>
2023-05-31 19:31:12 -0600

    se actualizó la vista del perfil y se creó el manager para cambiar foto de perfil

--- QuizApp/Views/ProfileView.swift
+++ QuizApp/Views/ProfileView.swift
@@ -2,28 +2,25 @@ import SwiftUI
 
 struct ProfileView: View {
     @State private var isEditing: Bool = false
-    @State private var name: String = "Usuario Test"
+    @State private var name: String = "Test User"
     @State private var email: String = "test@test.com"
-    @State private var age: String = "22 años"
+    @State private var age: String = "22 years old"
     @State private var auxName: String = ""
     @State private var auxEmail: String = ""
     @State private var auxiAge: String = ""
-    @State private var rank: String = "Principante"
+    @State private var rank: String = "Beginner"
     @State private var categoryFav: String = "Entertainment: Video Games"
+    @State private var profileImage: Image = Image(systemName: "person.fill")
+    @State private var showImagePicker: Bool = false
+    @State private var selectedImage: UIImage?
+    @State private var selectedImageTemp: UIImage?
     
     var body: some View {
         VStack {
-            Text("Mi Perfil")
+            Text("Profile")
                 .font(.largeTitle)
                 .bold()
-                .padding()
-            
-            Image(systemName: "person.fill")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 150, height: 150)
-                .padding()
-                .padding(.bottom, 10)
+                .padding(.vertical, 5)
             
             if isEditing {
                 editModeView()
@@ -42,40 +39,61 @@ struct ProfileView: View {
     private func editModeView() -> some View {
         VStack{
             VStack(alignment: .leading){
+                Button(action: {
+                    showImagePicker = true
+                }) {
+                    Text("Change Profile Picture")
+                            .padding()
+                            .background(Color("Beige"))
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                }
+                .sheet(isPresented: $showImagePicker, onDismiss: loadImage) {
+                    ImagePicker(image: $selectedImage)
+                }
                 VStack(alignment: .leading){
-                    Text("Nombre")
+                    Text("Name")
                         .font(.headline)
-                    TextField("Nombre", text: $auxName)
+                    TextField("", text: $auxName)
                         .font(.subheadline)
-                        .foregroundColor(Color("Beige")).opacity(0.8)
-                }.padding(.vertical, 10)
+                        .padding()
+                        .background(Color("WhiteBackground").opacity(0.1))
+                        .foregroundColor(Color("WhiteBackground"))
+                        .cornerRadius(10)
+                }.padding(.vertical, 5)
                 
                 VStack(alignment: .leading){
                     Text("Email")
                         .font(.headline)
-                    TextField("Email", text: $auxEmail)
+                    TextField("", text: $auxEmail)
                         .autocapitalization(.none)
                         .font(.subheadline)
-                        .foregroundColor(Color("Beige")).opacity(0.8)
-                }.padding(.vertical, 10)
+                        .padding()
+                        .background(Color("WhiteBackground").opacity(0.1))
+                        .foregroundColor(Color("WhiteBackground"))
+                        .cornerRadius(10)
+                }.padding(.vertical, 5)
                 
                 VStack(alignment:.leading){
-                    Text("Edad")
+                    Text("Age")
                         .font(.headline)
-                    TextField("Edad", text: $auxiAge)
-                        .font(.subheadline)
-                        .foregroundColor(Color("Beige")).opacity(0.8)
-                }.padding(.vertical, 10)
+                    TextField("", text: $auxiAge)
+                        .padding()
+                        .background(Color("WhiteBackground").opacity(0.1))
+                        .foregroundColor(Color("WhiteBackground"))
+                        .cornerRadius(10)
+                }.padding(.vertical, 5)
             }.padding()
             
             HStack {
                 Spacer()
                 Button(action: {
                     isEditing = false
+                    selectedImage = nil
                 }) {
-                    Text("Cancelar")
+                    Text("Cancel")
                         .padding()
-                        .background(Color.red)
+                        .background(Color("Beige").opacity(0.4))
                         .foregroundColor(.white)
                         .cornerRadius(10)
                 }
@@ -93,10 +111,13 @@ struct ProfileView: View {
                     if(!auxiAge.isEmpty){
                         age = auxiAge
                     }
+                    if let selectedImage = selectedImageTemp {
+                            profileImage = Image(uiImage: selectedImage)
+                        }
                 }) {
-                    Text("Guardar")
+                    Text("Save")
                         .padding()
-                        .background(Color.green)
+                        .background(Color("Beige"))
                         .foregroundColor(.white)
                         .cornerRadius(10)
                 }
@@ -105,24 +126,35 @@ struct ProfileView: View {
         }
     }
     
+    func loadImage() {
+        guard let selectedImage = selectedImage else { return }
+            selectedImageTemp = selectedImage
+    }
+    
     private func displayModeView() -> some View {
         VStack(alignment: .center) {
+            profileImage
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150)
+                .padding(.vertical, 5)
+                .clipShape(Circle())
             Text(name)
                 .font(.title)
             
             Text(email)
                 .font(.title3)
-                .foregroundColor(Color("Beige")).opacity(0.8)
+                .foregroundColor(Color("Beige"))
             Divider()
             VStack{
-                InfoRowView(title: "Edad", value: age)
-                InfoRowView(title: "Rango", value: rank)
-                InfoRowView(title: "Categoría Favorita", value: categoryFav)
+                InfoRowView(title: "Age", value: age)
+                InfoRowView(title: "Range", value: rank)
+                InfoRowView(title: "Favorite Category", value: categoryFav)
             }.padding()
             Button(action: {
                 isEditing = true
             }) {
-                Text("Editar")
+                Text("Edit")
                     .padding()
                     .background(Color("Beige"))
                     .foregroundColor(.white)
@@ -145,11 +177,11 @@ struct InfoRowView: View {
             if !value.isEmpty {
                 Text(value)
                     .font(.subheadline)
-                    .foregroundColor(Color("Beige")).opacity(0.8)
+                    .foregroundColor(Color("Beige"))
             } else {
                 Text("")
                     .font(.subheadline)
-                    .foregroundColor(Color("Beige")).opacity(0.8)
+                    .foregroundColor(Color("Beige"))
             }
         }
         .padding(.vertical, 10)

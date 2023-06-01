255356aa2ddebb75965c84513b6a88afc4f5daf8
Daniel López <hyperdaniel579exe@gmail.com>
2023-05-31 19:30:31 -0600

    se agregó nueva extensión para View

--- QuizApp/Extensions.swift
+++ QuizApp/Extensions.swift
@@ -8,3 +8,13 @@ extension Text {
             .foregroundColor(Color("WhiteBackground"))
     }
 }
+
+extension View{
+    func getRect() -> CGRect {
+        return UIScreen.main.bounds
+    }
+    
+    func getSafeArea() -> UIEdgeInsets{
+        return UIApplication.shared.windows.first?.safeAreaInsets ?? UIEdgeInsets(top:0, left: 0, bottom: 0, right: 0)
+    }
+}
